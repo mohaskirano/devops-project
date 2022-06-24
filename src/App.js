6c4f0106d88bc63cfc4ef9819c52a8c439ecfb85
mohamed <mohamed@localhost>
2022-06-24 12:50:16 +0000

    version2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3> MOHAMED MOCTAR MADANI </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
